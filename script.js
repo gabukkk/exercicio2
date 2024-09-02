8b52c7d696af9257d3bb38604bacf47b4e26d0f9
Gabriela <135336254+gabukkk@users.noreply.github.com>
2024-09-02 20:30:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 function verificarDia() {
-    var dia = document.getElementById('dia').value.toLowerCase();
-    var resposta;
+    var dia = document.getElementById('dia').value.toLowerCase().trim();
+    var respostaDia = document.getElementById('respostaDia');
 
     switch (dia) {
-        case 'segunda':
-        case 'terça':
-        case 'quarta':
-        case 'quinta':
-        case 'sexta':
-            resposta = 'É um dia útil.';
+        case "segunda":
+        case "segunda-feira":
+            respostaDia.innerHTML = "Segunda-feira, dia útil";
             break;
-        case 'sábado':
-        case 'domingo':
-            resposta = 'É fim de semana.';
+        
+        case "terça":
+        case "terça-feira":
+        case "terca-feira":
+        case "terca":
+            respostaDia.innerHTML = "Terça-feira, dia útil";
             break;
+        
+        case "quarta":
+        case "quarta-feira":
+            respostaDia.innerHTML = "Quarta-feira, dia útil";
+            break;
+        
+        case "quinta":
+        case "quinta-feira":
+            respostaDia.innerHTML = "Quinta-feira, dia útil";
+            break;
+        
+        case "sexta":
+        case "sexta-feira":
+            respostaDia.innerHTML = "Sexta-feira, dia útil";
+            break;
+        
+        case "sábado":
+        case "sabado":
+            respostaDia.innerHTML = "Sábado, fim de semana";
+            break;
+        
+        case "domingo":
+            respostaDia.innerHTML = "Domingo, fim de semana";
+            break;
+        
         default:
-            resposta = 'Dia inválido.';
+            respostaDia.innerHTML = "Digite outro.";
             break;
     }
-
-    document.getElementById('respostaDia').innerText = resposta;
 }
